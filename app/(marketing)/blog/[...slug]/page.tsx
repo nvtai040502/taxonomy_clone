af4f2555a1876ab124a67745502025c77baf37cd
Nguyễn Văn Tài <nvtai4502@gmail.com>
2023-12-20 08:54:14 +0700

    feat: blog page with contentlayer

---
+++ app/(marketing)/blog/[...slug]/page.tsx
@@ -0,0 +1,9 @@
+const generateMetadata = () => {
+  return ( 
+    <div>
+      Hello
+    </div>
+   );
+}
+ 
+export default generateMetadata;
---
+++ contentlayer.config.ts
@@ -0,0 +1,48 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+    date: {
+      type: "date",
+      required: true,
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    },
+    image: {
+      type: "string",
+      required: true,
+    },
+    authors: {
+      // Reference types are not embedded.
+      // Until this is fixed, we can use a simple list.
+      // type: "reference",
+      // of: Author,
+      type: "list",
+      of: { type: "string" },
+      required: true,
+    },
+  },
+  
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+    
+  },
+}))
+
+export default makeSource({ contentDirPath: "./content", documentTypes: [Post] })
